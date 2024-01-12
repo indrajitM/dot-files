f2e8256b4f545522283496808c5419274bd3a11b
Indrajit Mitra <indrajit.mitra@keysight.com>
2024-01-13 00:00:15 +0530

    LSP working.formating and linting yet to enable

--- .config/nvim/lua/imitra/lazy.lua
+++ .config/nvim/lua/imitra/lazy.lua
@@ -12,7 +12,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ { import = "imitra.plugins" },
-    -- { import = "imitra.plugins.lsp" }
+    { import = "imitra.plugins.lsp" }
 }, {
   install = {
     colorscheme = { "nightfly" },
--- .config/nvim/lua/imitra/plugins/harpoon.lua
+++ .config/nvim/lua/imitra/plugins/harpoon.lua
@@ -9,28 +9,28 @@ return {
     local harpoon = require("harpoon")
 
     -- basic telescope configuration
-    local conf = require("telescope.config").values
-    local function toggle_telescope(harpoon_files)
-        local file_paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-            table.insert(file_paths, item.value)
-        end
-
-        require("telescope.pickers").new({}, {
-            prompt_title = "Harpoon",
-            finder = require("telescope.finders").new_table({
-                results = file_paths,
-            }),
-            previewer = conf.file_previewer({}),
-            sorter = conf.generic_sorter({}),
-        }):find()
-    end
+    -- local conf = require("telescope.config").values
+    -- local function toggle_telescope(harpoon_files)
+    --     local file_paths = {}
+    --     for _, item in ipairs(harpoon_files.items) do
+    --         table.insert(file_paths, item.value)
+    --     end
+    --
+    --     require("telescope.pickers").new({}, {
+    --         prompt_title = "Harpoon",
+    --         finder = require("telescope.finders").new_table({
+    --             results = file_paths,
+    --         }),
+    --         previewer = conf.file_previewer({}),
+    --         sorter = conf.generic_sorter({}),
+    --     }):find()
+    -- end
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-    -- keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
     -- keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
     -- keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
     -- keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
@@ -40,8 +40,24 @@ return {
     keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
     keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
 
-    keymap.set("n", "<leader>h", function() toggle_telescope(harpoon:list()) end,
-    { desc = "Open harpoon window" })
+    -- keymap.set("n", "<leader>h", function() toggle_telescope(harpoon:list()) end,
+    -- { desc = "Open harpoon window" })
+
+    harpoon:extend({
+      UI_CREATE = function(cx)
+      keymap.set("n", "<C-v>", function()
+      harpoon.ui:select_menu_item({ vsplit = true })
+    end, { buffer = cx.bufnr })
+
+      keymap.set("n", "<C-x>", function()
+      harpoon.ui:select_menu_item({ split = true })
+    end, { buffer = cx.bufnr })
+
+      keymap.set("n", "<C-t>", function()
+      harpoon.ui:select_menu_item({ tabedit = true })
+    end, { buffer = cx.bufnr })
+  end,
+})
     --     keymap.set(
     --   "n",
     --   "<leader>hm",
